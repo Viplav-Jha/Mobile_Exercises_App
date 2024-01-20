79f3412af46f9029ae45977dafdb111e2553dde0
Viplav jha <viplavjha1993@gmail.com>
2024-01-20 20:36:44 +0530

    render exercises data with static json data

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import excises from './assets/data/exercises.json'
 
 export default function App() {
+  const excise = excises[0];
+  console.log(excise)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.exciseContainer}>
+      <Text style={styles.excisesName}>{excise.name}</Text>
+      <Text style={styles.exciseSubtitle}>{excise.muscle.toUpperCase()} | {excise.equipment.toUpperCase()}</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
+    padding:10,
   },
+  exciseContainer:{
+    backgroundColor:'#fff',
+    padding:10,
+    borderRadius:10,
+  },
+  excisesName:{
+    fontSize:20,
+    fontWeight:'500'
+  },
+  exciseSubtitle:{
+    color:'dimgray'
+  }
 });

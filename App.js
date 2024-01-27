e5dbc15f9c7fd43777fe86270c97a83c5bab34ee
Viplav jha <viplavjha1993@gmail.com>
2024-01-27 19:09:58 +0530

    comp created

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import excises from './assets/data/exercises.json'
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import excises from "./assets/data/exercises.json";
+import ExerciseListItem from "./src/components/ExerciseListItem";
 
 export default function App() {
-  const excise = excises[0];
-  console.log(excise)
   return (
-    <View style={styles.container}>
-      <View style={styles.exciseContainer}>
-      <Text style={styles.excisesName}>{excise.name}</Text>
-      <Text style={styles.exciseSubtitle}>{excise.muscle.toUpperCase()} | {excise.equipment.toUpperCase()}</Text>
-      </View>
-      
+    <View styles={styles.container}>
+      <FlatList
+        data={excises}
+        renderItem={({ item }) => <ExerciseListItem item={item} />}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -20,20 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gainsboro',
-    justifyContent: 'center',
-    padding:10,
+    backgroundColor: "gainsboro",
+    justifyContent: "center",
+    padding: 10,
   },
-  exciseContainer:{
-    backgroundColor:'#fff',
-    padding:10,
-    borderRadius:10,
-  },
-  excisesName:{
-    fontSize:20,
-    fontWeight:'500'
-  },
-  exciseSubtitle:{
-    color:'dimgray'
-  }
 });
